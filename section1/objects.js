3ce0a6ddcf355d426e57b08a0eb5d750b05d0d3f
rasheedhamza741 <rasheed.hamza741@gmail.com>
2024-10-04 17:34:37 +0530

    Objects2

--- section1/objects.js
+++ section1/objects.js
@@ -40,3 +40,74 @@ console.log(smartPhones.colors[1]);
 smartPhones.colors[0]='White;'
 smartPhones.colors.push('Green')
 console.log(smartPhones);
+
+const smartphoneArray = [
+{
+    Brand : 'Samsung',
+    model : 'Galaxy M31',
+    price : 13999,
+    colors : ['Black','Blue', 'Red'] ,
+},
+{
+    Brand : 'Oneplus',
+    model : '8T',
+    price : 41999,
+    colors : ['Black','Blue', 'Red'] ,
+},
+{
+    Brand : 'Apple',
+    model : 'iphone12',
+    price :  79900,
+    colors : ['Black','White', 'Green'] 
+},
+{
+    Brand : 'Xiaomi',
+    model : 'Redmi Note 9 pro',
+    price : 15999,
+    colors : ['Black','Blue', 'Red'] 
+},
+{
+    Brand : 'Realme',
+    model : 'Narzo 20 pro',
+    price : 18999,
+    colors : ['Black','whit', 'green'] 
+} ];
+
+console.log(smartphoneArray.lenght);
+console.log(smartphoneArray[1].price);
+
+// access 2nd color of third phone.
+console.log(smartphoneArray[2].colors[1]);
+
+//replace the first color of 4th phone with 'silver'
+smartphoneArray[3].colors[0]='silver'
+console.log(smartphoneArray[3]);
+
+//add a new color yellow to the 5th phone
+
+smartphoneArray[4].colors.push('Yellow');
+console.log(smartphoneArray[4]);
+
+const budgetPhones = smartphoneArray.filter((phone) =>{return phone.price>15000 && phone.price <20000})
+
+console.log(budgetPhones);
+
+const brands = smartphoneArray.map((phone) => {return phone.brand})
+console.log(brands);
+
+// filter all phone having 'Black' color
+
+console.log([1,2,3,4,5].includes(7));
+const blackPhones= smartphoneArray.filter((phone) => { return phone.colors.includes('Black') } );
+console.log(blackPhones);
+
+
+//filter all samsung phones
+
+const samsungPhones = smartphoneArray.filter((phone) =>{return phone.Brand === 'Samsung'});
+console.log(samsungPhones)
+
+const keyword = 'sa';
+const filteredPhones = smartphoneArray.filter((phone)=>{ return phone.Brand.toLowerCase().includes(keyword.toLowerCase())});
+console.log(filteredPhones);
+
